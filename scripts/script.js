1e6cedc9c703ce00ab624af426982d709e4515bc
Vasilsa1109 <vasilisavygovskaya1109@gmail.com>
2025-11-12 20:09:37 +0000

    add while

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ if (temp < 0) {
     console.log("на уице мороз")
 }
 
-let number = 2
+let number = 2;
 
 if (number%2==0) {
 console.log(number)
@@ -53,6 +53,10 @@ if (temp >= 30) {
 } else {
     console.log("холодно");
 }
+
+let age = 56;
+let hasTicket  = false;
+
 if (age >= 18 && hasTicket) {
     console.log("можно войти");
 } else if (age >= 18 && !hasTicket) {
@@ -102,3 +106,119 @@ let status = isOnline ? "в сети" : "не в сети";
 let process = ready ? "готов" : "не готов";
  console.log ("status:", process);
 
+for (let i = 1; i <= 5; i++) {
+    console.log ("повтор" + i);
+}
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i)
+}
+for (let i = 5; i <= 15; i++) {
+    console.log ("i");
+}
+for (let i = 5; i <= 1; i--) {
+    console.log ("i");
+}
+console.log ("старт");
+
+let j = 1;
+
+while (j <= 3) {
+    console.log ("шаг" + j);
+    j++;
+}
+
+let money = 100;
+
+while (money > 0) {
+    console.log ("покупаю мороженое");
+    money -= 20;
+}
+
+let i = 10;
+
+while (i > 0) {
+    console.log ("i");
+    i--;
+}
+
+let g = 1;
+
+do {
+    console.log(g);
+    g++;
+} while(g <= 3)
+
+let password;
+
+do {
+    password = prompt("введите пароль");
+} while(password !== "1234");
+console.log("пароль принят"); 
+
+let num;
+do{
+    num = prompt("введите число");
+} while(num != 5); 
+
+let fruits = ["яблоко","банан","груша"];
+
+for (let fruit of fruits) {
+    console.log ("fruit");
+}
+
+for (let better of "привет") {
+    console.log (better);
+}
+let animals = ["кот","собака","хомяк", "лиса"];
+
+for(let animal of animals)
+ {
+    console.log ("animals");
+}
+
+let person = {name: "Ann", age: 15, city: "minsk"};
+
+for (let key in person) {
+    console.log (key + ": " + person[key])
+}
+
+let me = {name: "Arina", age: 14, city: "minsk"};
+
+for (let key in me) {
+    console.log (key + ": " + me[key])
+}
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) continue;
+    console.log(i);
+}
+for (let i = 1; i <= 10; i++) {
+    if (i%2 === 3) continue;
+    console.log(i);
+}
+
+let sum = 0;
+
+for(i=1; i<= 5; i++){
+    sum += i;
+}
+console.log("сумма", sum);
+
+for (let i=1; i <= 10; i++){
+    console.log (`3 * ${1} = ${3 * i}`)
+}
+
+let word = "банан";
+let count = 0;
+
+for (let letter of word) {
+    if (letter === "a") count++;
+}
+console.log(`букв в слове ${word}`, count);
+
+let fruitword= "ананас";
+let fruitwordcount = 0;
+
+for (let letter of fruitword.toLowerCase()) {
+    if (letter === "a") fruitwordcount++;
+}
+console.log(`букв в слове ${fruitword}`, fruitwordcount)
